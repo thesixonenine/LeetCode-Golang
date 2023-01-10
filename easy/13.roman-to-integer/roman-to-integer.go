a01cf82d71702e54cf049bcadf3358b75d7584f2
Simple <huyang1994822@gmail.com>
2023-01-10 23:38:10 +0800

    add 13.roman-to-integer

--- easy/13.roman-to-integer/roman-to-integer.go
+++ easy/13.roman-to-integer/roman-to-integer.go
@@ -1,1 +1,34 @@
 package roman_to_integer
+
+func romanToInt(s string) int {
+	byteValueMap := map[byte]int{
+		'I': 1,
+		'V': 5,
+		'X': 10,
+		'L': 50,
+		'C': 100,
+		'D': 500,
+		'M': 1000,
+	}
+	bytes := []byte(s)
+	l := len(bytes)
+	r := 0
+	for i, b := range bytes {
+		v := byteValueMap[b]
+		if i+1 < l {
+			// 不是最后一个数字
+			next := bytes[i+1]
+			if b == 'I' && (next == 'V' || next == 'X') {
+				v = 0 - v
+			}
+			if b == 'X' && (next == 'L' || next == 'C') {
+				v = 0 - v
+			}
+			if b == 'C' && (next == 'D' || next == 'M') {
+				v = 0 - v
+			}
+		}
+		r = r + v
+	}
+	return r
+}
--- easy/13.roman-to-integer/roman-to-integer_test.go
+++ easy/13.roman-to-integer/roman-to-integer_test.go
@@ -1,1 +1,18 @@
 package roman_to_integer
+
+import "testing"
+
+func TestRomanToInt(t *testing.T) {
+	intMap := map[string]int{}
+	intMap["III"] = 3
+	intMap["IV"] = 4
+	intMap["IX"] = 9
+	intMap["LVIII"] = 58
+
+	for roman, want := range intMap {
+		got := romanToInt(roman)
+		if got != want {
+			t.Errorf("got '%v' want '%v'", got, want)
+		}
+	}
+}
