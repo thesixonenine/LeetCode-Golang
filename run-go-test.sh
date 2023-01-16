d5877051f63b240103e6123938b5c859a2824ade
Simple <huyang1994822@gmail.com>
2023-01-16 17:50:25 +0800

    add test bash

--- run-go-test.sh
+++ run-go-test.sh
@@ -2,16 +2,17 @@
 
 function read_dir(){
     hasTestFile=0
-    for file in `ls $1`       #注意此处这是两个反引号，表示运行系统命令
+    for file in `ls $1`
     do
-        if [ -d $1"/"$file ]  #注意此处之间一定要加上空格，否则会报错
+        if [ -d $1"/"$file ]
         then
             read_dir $1"/"$file
         else
-            #echo "/\\"   #在此处处理文件即可
             if [[ $file =~ .*_test.go$ ]]; then
                 echo $1"/"$file
+                cd $1"/"
                 go test
+                cd -
             fi
         fi
     done
