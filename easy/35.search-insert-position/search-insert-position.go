1e6160ef9f24812bbba4d84b923e56d2fd5993fb
Simple <huyang1994822@gmail.com>
2023-01-18 17:00:43 +0800

    update 35.search-insert-position

--- easy/35.search-insert-position/search-insert-position.go
+++ easy/35.search-insert-position/search-insert-position.go
@@ -2,5 +2,20 @@ package search_insert_position
 
 // https://leetcode.com/problems/search-insert-position/
 func searchInsert(nums []int, target int) int {
-	return 0
+	// 在一个有序数组中找第一个大于等于 target 的元素的下标
+	ans := len(nums)
+	r := ans - 1
+	l := 0
+	var mid int
+	for l <= r {
+		mid = (l + r) / 2
+		if nums[mid] >= target {
+			ans = mid
+			r = mid - 1
+		} else {
+			// 查右边
+			l = mid + 1
+		}
+	}
+	return ans
 }
--- easy/35.search-insert-position/search-insert-position_test.go
+++ easy/35.search-insert-position/search-insert-position_test.go
@@ -10,4 +10,20 @@ func TestSearchInsert(t *testing.T) {
 	if got != want {
 		t.Errorf("got '%v' want '%v'", got, want)
 	}
+
+	nums = []int{1, 3, 5, 6}
+	target = 2
+	want = 1
+	got = searchInsert(nums, target)
+	if got != want {
+		t.Errorf("got '%v' want '%v'", got, want)
+	}
+
+	nums = []int{1, 3, 5, 6}
+	target = 7
+	want = 4
+	got = searchInsert(nums, target)
+	if got != want {
+		t.Errorf("got '%v' want '%v'", got, want)
+	}
 }
