fd1b921fd82ea322b733612724900def4545ed3f
Simple <huyang1994822@gmail.com>
2023-01-18 17:33:39 +0800

    add test 58.length-of-last-word

--- easy/58.length-of-last-word/length-of-last-word.go
+++ easy/58.length-of-last-word/length-of-last-word.go
@@ -2,5 +2,10 @@ package length_of_last_word
 
 // https://leetcode.com/problems/length-of-last-word/
 func lengthOfLastWord(s string) int {
+	bytes := []byte(s)
+	l := len(bytes)
+	for i := l - 1; i >= 0; i++ {
+
+	}
 	return 0
 }
