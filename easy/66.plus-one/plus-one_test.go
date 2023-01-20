ee4aae30343b8e148bfeeab0f0a6a977806bb589
Simple <huyang1994822@gmail.com>
2023-01-20 11:09:10 +0800

    add test

--- easy/66.plus-one/plus-one_test.go
+++ easy/66.plus-one/plus-one_test.go
@@ -3,5 +3,15 @@ package plus_one
 import "testing"
 
 func TestPlusOne(t *testing.T) {
-	
+	testArr := []int{1, 2, 3}
+	wantArr := []int{1, 2, 4}
+	got := plusOne(testArr)
+	if len(wantArr) != len(got) {
+		t.Errorf("got length '%v' want length '%v'", len(got), len(wantArr))
+	}
+	for i, item := range wantArr {
+		if item != got[i] {
+			t.Errorf("got '%v' want '%v'", got[i], item)
+		}
+	}
 }
