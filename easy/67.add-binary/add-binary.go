48da7a2494e23fa5fd6e517a325621eb216aee48
Simple <huyang1994822@gmail.com>
2023-01-31 23:44:21 +0800

    add test

---
+++ easy/67.add-binary/add-binary.go
@@ -0,0 +1,6 @@
+package add_binary
+
+// https://leetcode.com/problems/add-binary/
+func addBinary(a string, b string) string {
+	return ""
+}
---
+++ easy/67.add-binary/add-binary_test.go
@@ -0,0 +1,23 @@
+package add_binary
+
+import (
+	"testing"
+)
+
+func TestAddBinary(t *testing.T) {
+	a := "11"
+	b := "1"
+	want := "100"
+	got := addBinary(a, b)
+	if got != want {
+		t.Errorf("got '%v' want '%v'", got, want)
+	}
+
+	a = "1010"
+	b = "1011"
+	want = "10101"
+	got = addBinary(a, b)
+	if got != want {
+		t.Errorf("got '%v' want '%v'", got, want)
+	}
+}
