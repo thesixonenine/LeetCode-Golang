95c704b45dc5916fbe13ce16b542d5fa6ead24da
Simple <huyang1994822@gmail.com>
2023-02-03 23:33:04 +0800

    add 83.remove-duplicates-from-sorted-list

--- easy/83.remove-duplicates-from-sorted-list/remove-duplicates-from-sorted-list_test.go
+++ easy/83.remove-duplicates-from-sorted-list/remove-duplicates-from-sorted-list_test.go
@@ -3,5 +3,23 @@ package remove_duplicates_from_sorted_list
 import "testing"
 
 func TestDeleteDuplicates(t *testing.T) {
+	l1 := &ListNode{Val: 1}
+	l2 := &ListNode{Val: 1}
+	l3 := &ListNode{Val: 2}
+	l1.Next = l2
+	l2.Next = l3
 
+	want := &ListNode{Val: 1}
+	w1 := &ListNode{Val: 2}
+	want.Next = w1
+
+	got := deleteDuplicates(l1)
+
+	for want != nil {
+		if want.Val != got.Val {
+			t.Errorf("got '%v' want '%v'", got.Val, want.Val)
+		}
+		want = want.Next
+		got = got.Next
+	}
 }
