568c9712cb269f0fba43f7a0f494ac87e2b0faf6
Simple <huyang1994822@gmail.com>
2023-02-03 15:36:22 +0800

    add test

---
+++ easy/83.remove-duplicates-from-sorted-list/remove-duplicates-from-sorted-list.go
@@ -0,0 +1,17 @@
+package remove_duplicates_from_sorted_list
+
+type ListNode struct {
+	Val  int
+	Next *ListNode
+}
+
+/**
+ * Definition for singly-linked list.
+ * type ListNode struct {
+ *     Val int
+ *     Next *ListNode
+ * }
+ */
+func deleteDuplicates(head *ListNode) *ListNode {
+	return head
+}
---
+++ easy/83.remove-duplicates-from-sorted-list/remove-duplicates-from-sorted-list_test.go
@@ -0,0 +1,7 @@
+package remove_duplicates_from_sorted_list
+
+import "testing"
+
+func TestDeleteDuplicates(t *testing.T) {
+
+}
