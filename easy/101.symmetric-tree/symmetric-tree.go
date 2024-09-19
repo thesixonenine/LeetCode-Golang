06a195a58af3f6a248b8e94f55b514ebac71376c
Simple <huyang1994822@gmail.com>
2024-09-19 18:07:41 +0800

    add 101.symmetric-tree

--- easy/101.symmetric-tree/symmetric-tree.go
+++ easy/101.symmetric-tree/symmetric-tree.go
@@ -7,5 +7,17 @@ type TreeNode struct {
 }
 
 func isSymmetric(root *TreeNode) bool {
-	return false
+	if root == nil {
+		return true
+	}
+	return isMirror(root.Left, root.Right)
+}
+func isMirror(left, right *TreeNode) bool {
+	if left == nil && right == nil {
+		return true
+	}
+	if left == nil || right == nil {
+		return false
+	}
+	return left.Val == right.Val && isMirror(left.Left, right.Right) && isMirror(left.Right, right.Left)
 }
