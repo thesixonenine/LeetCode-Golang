3d550b9b4b78be43a06c9d5186cf56bf904273cf
Simple <huyang1994822@gmail.com>
2024-09-20 17:55:46 +0800

    WIP: 108.convert-sorted-array-to-binary-search-tree

---
+++ easy/108.convert-sorted-array-to-binary-search-tree/convert-sorted-array-to-binary-search-tree.go
@@ -0,0 +1,11 @@
+package convert_sorted_array_to_binary_search_tree
+
+type TreeNode struct {
+	Val   int
+	Left  *TreeNode
+	Right *TreeNode
+}
+
+func sortedArrayToBST(nums []int) *TreeNode {
+	return nil
+}
