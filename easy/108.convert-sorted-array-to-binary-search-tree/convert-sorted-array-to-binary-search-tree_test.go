7b2e5d49a79446826c29dc3cec5bdfdd60517ca5
Simple <huyang1994822@gmail.com>
2024-09-20 17:55:46 +0800

    WIP: 108.convert-sorted-array-to-binary-search-tree

---
+++ easy/108.convert-sorted-array-to-binary-search-tree/convert-sorted-array-to-binary-search-tree_test.go
@@ -0,0 +1,27 @@
+package convert_sorted_array_to_binary_search_tree
+
+import (
+	"reflect"
+	"testing"
+)
+
+func Test_sortedArrayToBST(t *testing.T) {
+	type args struct {
+		nums []int
+	}
+	tests := []struct {
+		name string
+		args args
+		want *TreeNode
+	}{
+		{name: "case1", args: args{nums: []int{-10, -3, 0, 5, 9}}, want: nil},
+		{name: "case2", args: args{nums: []int{1, 3}}, want: nil},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := sortedArrayToBST(tt.args.nums); !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("sortedArrayToBST() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
